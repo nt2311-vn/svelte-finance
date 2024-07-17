fb313afab6e3a69da59adcfff5ac943a166047c8
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-17 22:23:56 +0700

    Header components

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { HomeIcon, StoreIcon, Receipt, Icon } from 'lucide-svelte';
+	import type { ComponentType } from 'svelte';
+	import { Button } from './ui/button';
+
+	type Feature = {
+		label: string;
+		icon: ComponentType<Icon>;
+		relPath: string;
+	};
+
+	const featureLists: Feature[] = [
+		{ label: 'Home', icon: HomeIcon, relPath: '/' },
+		{ label: 'Stock', icon: StoreIcon, relPath: '/stock' },
+		{ label: 'Pricing', icon: Receipt, relPath: '/pricing' }
+	];
+</script>
+
+<header class="w-full">
+	<div class="flex h-14 items-center px-12 py-8">
+		<nav class="flex flex-col space-x-4 md:flex-row md:space-x-4 md:space-y-0">
+			{#each featureLists as fList}
+				<Button href={fList.relPath}>
+					<fList.icon class="size-4" />
+					<span class="bg-primary font-semibold text-white">
+						{fList.label}
+					</span>
+				</Button>
+			{/each}
+		</nav>
+	</div>
+</header>

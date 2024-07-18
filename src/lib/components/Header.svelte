8026abc028a8b0bec99ca4c9f1df17247a3a697e
nt2311-vn <nguyentoan231196@gmail.com>
2024-07-18 22:33:13 +0700

    Header components

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,61 @@
+<div class="navbar bg-indigo-950">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl" href="/">daisyUI</a>
+	</div>
+	<div class="flex-none gap-2">
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+		</div>
+		<div class="dropdown dropdown-end">
+			<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img
+						alt="Tailwind CSS Navbar component"
+						src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+					/>
+				</div>
+			</div>
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+			>
+				<li><a>Settings</a></li>
+				<li><a>Logout</a></li>
+				<li>
+					<label class="flex cursor-pointer gap-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<circle cx="12" cy="12" r="5" />
+							<path
+								d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
+							/>
+						</svg>
+						<input type="checkbox" value="light" class="toggle theme-controller" />
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+						</svg>
+					</label>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
